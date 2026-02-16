d43b27ffcb04fe4d801f429d13e2ee17c97ab300
DimasAji <ajid0818@gmail.com>
2026-02-16 09:05:08 +0700

    fix: correct import path for ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AppRouter } from './app/router'
-import { ThemeProvider } from './hooks/useTheme'
+import { ThemeProvider } from './hooks/ThemeProvider'
 import { Toaster } from './components/ui/sonner'
 import './styles/index.css'
 
